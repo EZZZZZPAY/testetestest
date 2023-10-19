dd0e3b4a4267a2dcc9040a085c67bcf8d3fcba61
EZZZZZ <52199610+EZZZZZPAY@users.noreply.github.com>
2023-10-19 13:29:36 +0200

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+// setting time zone
+date_default_timezone_set("Europe/Warsaw");
+
+// define the site root
+define('SITE_ROOT', 'http://localhost/ample/');
+
+
+
+// Database Information
+// Database Hostname
+define('DATABASE_HOST','localhost');
+// Database Username
+define('DATABASE_USER','root');
+// Database Name
+define('DATABASE_NAME', 'dash2');
+// Database DB_PASS
+define('DATABASE_PASS','');
+
+
+ ?>
